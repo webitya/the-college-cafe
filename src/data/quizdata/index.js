9d0d12ddb46e49776be0fb22802a11440d65f4fe
Aditya Kumar <webitya@gmail.com>
2025-08-30 22:24:35 +0530

    Aditya

--- src/data/quizdata/index.js
+++ src/data/quizdata/index.js
@@ -1,11 +1,13 @@
 import { week1Quiz } from "./week1/quiz.js"
 import { week2Quiz } from "./week2/quiz.js"
 import { week3Quiz } from "./week3/quiz.js"
+import { week4Quiz } from "./week4/quiz.js"
 
 export const quizData = {
   week1: week1Quiz,
   week2: week2Quiz,
   week3: week3Quiz,
+   week4: week4Quiz,
 }
 
 export const getAvailableWeeks = () => {
